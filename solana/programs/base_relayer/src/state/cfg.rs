2f1b146863b1d2e6d96fd035f5ac46a3ea52d42f
Jack Chuma <jack.chuma@coinbase.com>
2025-09-06 09:51:17 -0400

    re-order nonce in cfg struct

--- solana/programs/base_relayer/src/state/cfg.rs
+++ solana/programs/base_relayer/src/state/cfg.rs
@@ -5,12 +5,12 @@ use crate::internal::{Eip1559, GasConfig};
 #[account]
 #[derive(Debug, PartialEq, Eq, InitSpace)]
 pub struct Cfg {
+    /// Canonical nonce
+    pub nonce: u64,
     /// Guardian pubkey authorized to update configuration
     pub guardian: Pubkey,
     /// EIP-1559 state and configuration for dynamic pricing.
     pub eip1559: Eip1559,
     /// Gas configuration
     pub gas_config: GasConfig,
-    /// Canonical nonce
-    pub nonce: u64,
 }
